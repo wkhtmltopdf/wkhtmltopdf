38904018465894d6ddc87910851cbb455f95e6b2
Jakob Truelsen <jakobt@geneshaft.antialize.dk>
2009-09-29 19:58:13 +0200

    Started migration of code to pageconverter

---
+++ src/textualfeedback.hh
@@ -0,0 +1,28 @@
+//-*- mode: c++; tab-width: 4; indent-tabs-mode: t; c-file-style: "stroustrup"; -*-
+// This file is part of wkhtmltopdf.
+//
+// wkhtmltopdf is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// wkhtmltopdf is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with wkhtmltopdf.  If not, see <http://www.gnu.org/licenses/>.
+#ifndef __TEXTUALFEEDBACK_HH__
+#define __TEXTUALFEEDBACK_HH__
+#include "pageconverter.hh"
+class TextualFeedbackPrivate;
+
+class TextualFeedback: public Feedback {
+public:
+	TextualFeedback(FILE * fd);
+	~TextualFeedback();
+private:
+	TextualFeedbackPrivate * d;
+};
+#endif //__TEXTUALFEEDBACK_HH__
