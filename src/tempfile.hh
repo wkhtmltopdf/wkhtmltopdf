48df16424d538491a7eed2d495351e1573c9b12d
Antialize <antialize@gmail.com>
2009-10-02 10:25:29 +0200

    Abstract out temporery files

---
+++ src/tempfile.hh
@@ -0,0 +1,29 @@
+//-*- mode: c++; tab-width: 4; indent-tabs-mode: t; c-file-style: "stroustrup"; -*-
+// This file is part of wkhtmltopdf.
+//
+// wkhtmltopdf is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// wkhtmltopdf is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with wkhtmltopdf.  If not, see <http://www.gnu.org/licenses/>.
+#ifndef __TEMPFILE_HH__
+#define __TEMPFILE_HH__
+
+class TempFile {
+private:
+	QString path;
+public:
+	TempFile();
+	~TempFile();
+	QString create(const QString & ext);
+	void remove();
+};
+
+#endif //__TEMPFILE_HH__
