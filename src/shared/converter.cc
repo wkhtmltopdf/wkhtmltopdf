2b45922c0ce51aa222202af641d4a8a10eed7269
Jakob Truelsen <antialize@gmail.com>
2010-06-20 21:12:37 +0200

    Include QWebFrame in converter.cc so that settings have an effect

--- src/shared/converter.cc
+++ src/shared/converter.cc
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with wkhtmltopdf.  If not, see <http://www.gnu.org/licenses/>.
 #include "converter_p.hh"
+#include <QWebFrame>
 #include "multipageloader.hh"
 #include <qapplication.h>
 namespace wkhtmltopdf {
