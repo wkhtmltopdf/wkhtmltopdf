wkhtmltopdf/wkhtmltopdf

commit ce6d6cd0f0f86a5b1ff3765aaae357dfdf3be803

Network (or equivalent) level errors throwing during the loading of a resource (such trying to access an invalid URL) were not being detailed reported. I've added code to take into account QNetworkReply->error() along with HTTP's statuses.

src/lib/multipageloader.cc
@@ -338,19 +338,28 @@ void ResourceObject::error(const QString & str) {
  * \param reply The networkreply that has finished
  */
 void ResourceObject::amfinished(QNetworkReply * reply) {
-	int errorCode = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
-	if (errorCode > 399 && httpErrorCode == 0)
+	int networkStatus = reply->error();
+	int httpStatus = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+	if (networkStatus > 0 || (httpStatus > 399 && httpErrorCode == 0))
 	{
 		QFileInfo fi(reply->url().toString());
 		bool mediaFile = settings::LoadPage::mediaFilesExtensions.contains(fi.completeSuffix().toLower());
 		if ( ! mediaFile) {
-			httpErrorCode = errorCode;
+			// XXX: Notify network errors as higher priority than HTTP errors.
+			//      QT's QNetworkReply::NetworkError enum uses values overlapping
+			//      HTTP status codes, so adding 1000 to QT's codes will avoid
+			//      confusion. Also a network error at this point will probably mean
+			//      no HTTP access at all, so we want network errors to be reported
+			//      with a higher priority than HTTP ones. (pruiz)
+			//      See: http://doc-snapshot.qt-project.org/4.8/qnetworkreply.html#NetworkError-enum
+			httpErrorCode = networkStatus > 0 ? (networkStatus + 1000) : httpStatus;
 			return;
 		}
 		if (settings.mediaLoadErrorHandling == settings::LoadPage::abort)
 		{
-			httpErrorCode = errorCode;
-			error(QString("Failed to load ") + reply->url().toString() + " (sometimes it will work just to ignore this error with --load-media-error-handling ignore)");
+			httpErrorCode = networkStatus > 0 ? (networkStatus + 1000) : httpStatus;
+			error(QString("Failed to load ") + reply->url().toString() + ", with code: " + QString::number(httpErrorCode) + 
+				" (sometimes it will work just to ignore this error with --load-media-error-handling ignore)");
 		}
 		else {
 			warning(QString("Failed to load %1 (%2)")

commit efbc7235c4239294e8de56821ad6bb2bf997bf1e

fix spurious "exit with code 1 due to http error: 1xxx" errors
This fixes #1502 and was introduced in ce6d6cd0f0f86a5b1ff3765aaae357dfdf3be803,
which returned errors above 1000 as a proxy for network errors. However, the
error 5 (i.e. OperationCanceledError) was not handled, which apparently happens
a lot due to parallel loading of resources. We thus ignore this error in the
loader.

In addition, in case the error is greater than 1000, we find out the correct
network error and display that error instead of HTTP error 1xxx which doesn't
exist. The trick to find out the text values for the enum was taken from:

https://blog.qt.digia.com/blog/2008/10/09/coding-tip-pretty-printing-enum-values/

src/lib/multipageloader.cc
@@ -335,7 +335,7 @@ void ResourceObject::error(const QString & str) {
 void ResourceObject::amfinished(QNetworkReply * reply) {
 	int networkStatus = reply->error();
 	int httpStatus = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
-	if (networkStatus > 0 || (httpStatus > 399 && httpErrorCode == 0))
+	if ((networkStatus != 0 && networkStatus != 5) || (httpStatus > 399 && httpErrorCode == 0))
 	{
 		QFileInfo fi(reply->url().toString());
 		bool mediaFile = settings::LoadPage::mediaFilesExtensions.contains(fi.completeSuffix().toLower());

src/lib/utilities.cc
@@ -27,6 +27,8 @@
 #include "utilities.hh"
 #include <QDebug>
 #include <QTextStream>
+#include <QMetaEnum>
+#include <QNetworkReply>
 
 void loadSvg(QSvgRenderer * & ptr, const QString & path, const char * def, int w, int h) {
 	 delete ptr;
@@ -160,7 +162,21 @@ int handleError(bool success, int errorCode) {
 		if (ce.contains(errorCode)) c = ce[errorCode];
 		const char * m = "";
 		if (cm.contains(errorCode)) m = cm[errorCode];
-		fprintf(stderr, "Exit with code %d due to http error: %d %s\n", c, errorCode, m);
+		if (errorCode < 1000) {
+			fprintf(stderr, "Exit with code %d due to http error: %d %s\n", c, errorCode, m);
+		} else {
+			QNetworkReply::NetworkError error = (QNetworkReply::NetworkError)(errorCode - 1000);
+			QString errorValue;
+			QMetaObject meta = QNetworkReply::staticMetaObject;
+			for (int i=0; i < meta.enumeratorCount(); ++i) {
+				QMetaEnum m = meta.enumerator(i);
+				if (m.name() == QLatin1String("NetworkError")) {
+					errorValue = QLatin1String(m.valueToKey(error));
+					break;
+				}
+			}
+			fprintf(stderr, "Exit with code %d due to network error: %s\n", c, errorValue.toLocal8Bit().data());
+		}
 		return c;
 	} else if (!success) {
 		fprintf(stderr, "Exit with code %d, due to unknown error.\n", EXIT_FAILURE);