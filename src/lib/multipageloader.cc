ffb77c92925691678ddbf68e32e2c42da2af87f4
Peter van der Tak <pta@ibuildgreen.eu>
2015-07-14 19:58:29 +0200

    Fix device pixel ratio in CSS media queries.

--- src/lib/multipageloader.cc
+++ src/lib/multipageloader.cc
@@ -216,6 +216,7 @@ ResourceObject::ResourceObject(MultiPageLoaderPrivate & mpl, const QUrl & u, con
 
 	double devicePixelRatio = multiPageLoader.dpi / 96.; // The used version of WebKit always renders at 96 DPI when no zoom is applied. It does not fully support a device pixel ratio != 1 natively.
 	webPage.mainFrame()->setZoomFactor(devicePixelRatio * settings.zoomFactor); // Zoom in the page to achieve a higher DPI.
+	webPage.setDevicePixelRatio(devicePixelRatio); // Fix CSS media queries (does not affect anything else).
 }
 
 /*!
