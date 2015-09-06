e828fee4111ba95bc502b0d4477cf48c8d7d9efe
Fr33m1nd <lukion@gmx.de>
2015-09-06 20:28:42 +0200

    Allow email-adress as username
    Apparently a email-adress as username does not work, should use strrchar instead

--- src/lib/loadsettings.cc
+++ src/lib/loadsettings.cc
@@ -80,7 +80,7 @@ Proxy strToProxy(const char * proxy, bool * ok) {
 	}
 
 	//Read username and password
-	char * val = (char *) strchr(proxy,'@');
+	char * val = (char *) strrchr(proxy,'@');
 	p.user = p.password = "";
 	if (val != NULL) {
 		p.user = QString(proxy).left(val-proxy);
