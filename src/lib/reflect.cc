b6646f1e528284faa4fe7f79f8330f6f388967b6
JasonParallel <jasonparallel@gmail.com>
2016-12-09 17:38:15 -0500

    Code Review Formatting Correction
    Code Review Formatting Correction

--- src/lib/reflect.cc
+++ src/lib/reflect.cc
@@ -51,15 +51,15 @@ ReflectClass::~ReflectClass() {
 }
 
 ReflectImpl<LoadGlobal>::ReflectImpl(LoadGlobal & c) {
-    WKHTMLTOPDF_REFLECT(cookieJar);
+	WKHTMLTOPDF_REFLECT(cookieJar);
 }
 
 ReflectImpl<LoadPage>::ReflectImpl(LoadPage & c) {
 	WKHTMLTOPDF_REFLECT(username);
 	WKHTMLTOPDF_REFLECT(password);
-    WKHTMLTOPDF_REFLECT(clientSslKeyPath);
-    WKHTMLTOPDF_REFLECT(clientSslKeyPassword);
-    WKHTMLTOPDF_REFLECT(clientSslCrtPath);
+	WKHTMLTOPDF_REFLECT(clientSslKeyPath);
+	WKHTMLTOPDF_REFLECT(clientSslKeyPassword);
+	WKHTMLTOPDF_REFLECT(clientSslCrtPath);
 	WKHTMLTOPDF_REFLECT(jsdelay);
 	WKHTMLTOPDF_REFLECT(windowStatus);
 	WKHTMLTOPDF_REFLECT(zoomFactor);
